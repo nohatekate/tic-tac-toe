9b211b70a657c78a64698f6a3989373e9ed278a8
nohatekate <kateiscoding@gmail.com>
2023-02-28 16:38:00 -0600

    variable setup

--- js/main.js
+++ js/main.js
@@ -1,12 +1,13 @@
   /*----- constants -----*/
+// 1) Define required constants
 //1) Define required constants:
+//1.1
 const colors = {
-    '1': null,
-    '-1': null
+    'player1': null,
+    'player-1': null
 }
-let currentPlayer = 'X'
 
-//1.2
+    //1.2
 const winningCombinations = {
     combo1 :["0", "1", "2"],
     combo2 :["0", "4", "8"],  
@@ -17,55 +18,32 @@ const winningCombinations = {
     combo7:["1", "4", "7"],
     combo8:["2", "5", "8"]
 }
-
   /*----- state variables -----*/
-//   2) Define required variables used to track the state of the game:
+// 2) Define required variables used to track the state of the game
+const gameBoard = [0, 1, 2, 3, 4, 5, 6, 7, 8]; 
 
-  /*----- cached elements  -----*/
-//   3) Store elements on the page that will be accessed in code more than once in variables to make code more concise, readable and performant:
+let yourTurn = null;
+
+let winner = null;
+
+// 3) Store elements on the page that will be accessed in code more than once in variables to make code more concise, readable and performant.
+const square0 = document.querySelector('#0');
+const square1 = document.querySelector('#1');
+const square2 = document.querySelector('#2');
+const square3 = document.querySelector('#3');
+const square4 = document.querySelector('#4');
+const square5 = document.querySelector('#5');
+const square6 = document.querySelector('#6');
+const square7 = document.querySelector('#7');
+const square8 = document.querySelector('#8');
 
   /*----- event listeners -----*/
-//   4) Upon loading the app should:
-
-const squareEls = document.querySelectorAll(".square")
-console.log(squareEls)
+// 4) Upon loading the app should:
+//   4.1) Initialize the state variables
+//   4.2) Render those values to the page
+//   4.3) Wait for the user to click a square
 
   /*----- functions -----*/
-//   5) Handle a player clicking a square
-function handleClick(evt){
-    if(currentPlayer === 'X' && evt.target.innerHTML === ""){
-        evt.target.innerHTML = "X"
-        currentPlayer = "O"
-        console.log(currentPlayer)
-    } else if(currentPlayer = "O" && evt.target.innerHTML === "") {
-        evt.target.innerHTML = "O"
-        currentPlayer = "X"
-        console.log(currentPlayer)
-    } 
-// console.log(evt.target.innerText = "X")
-}
+// 5) Handle a player clicking a square
 
-
-
-squareEls.forEach(function(evt){
-  evt.addEventListener("click", handleClick)
-})
-// for (let i = 0; i<squareEls.length; i++){
-//     evt.addEventListener("click", handleClick(squareEls[i]))
-//     // handleClick(squareEls[i])
-//     console.log(squareEls[i])
-// }
-// 6) Handle a player clicking the replay button:
-const resetButton = document.querySelector('#button')
-console.log(resetButton)
-resetButton.addEventListener("click", resetGame)
-function resetGame(){
-    squareEls.forEach(square)
-      
-    
-    console.log("I've been clicked")
-}
-function square (square){
-    square.innerHTML = ""
-    console.log("We're making progress")
-}
+// 6) Handle a player clicking the replay button
