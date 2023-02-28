5c0b29e5a85d9040865fdb331b349112cfe6b4de
nohatekate <kateiscoding@gmail.com>
2023-02-27 18:36:06 -0600

    Tic Tac Toe - Display empty board, Player can click, Click alternates XO

---
+++ js/main.js
@@ -0,0 +1,56 @@
+  /*----- constants -----*/
+//1) Define required constants:
+const colors = {
+    '1': null,
+    '-1': null
+}
+let currentPlayer = 'X'
+
+//1.2
+const winningCombinations = {
+    combo1 :["0", "1", "2"],
+    combo2 :["0", "4", "8"],  
+    combo3 :["0", "3", "6"],
+    combo4 :["3", "4", "5"],
+    combo5 :["6", "7", "8"],
+    combo6 :["6", "4", "2"],
+    combo7:["1", "4", "7"],
+    combo8:["2", "5", "8"]
+}
+  /*----- state variables -----*/
+//   2) Define required variables used to track the state of the game:
+
+  /*----- cached elements  -----*/
+//   3) Store elements on the page that will be accessed in code more than once in variables to make code more concise, readable and performant:
+
+  /*----- event listeners -----*/
+//   4) Upon loading the app should:
+
+const squareEls = document.querySelectorAll(".square")
+console.log(squareEls)
+
+  /*----- functions -----*/
+//   5) Handle a player clicking a square
+function handleClick(evt){
+    if(currentPlayer === 'X' && evt.target.innerHTML === ""){
+        evt.target.innerHTML = "X"
+        currentPlayer = "O"
+        console.log(currentPlayer)
+    } else if(currentPlayer = "O" && evt.target.innerHTML === "") {
+        evt.target.innerHTML = "O"
+        currentPlayer = "X"
+        console.log(currentPlayer)
+    } 
+// console.log(evt.target.innerText = "X")
+}
+
+squareEls.forEach(function(evt){
+  evt.addEventListener("click", handleClick)
+})
+// for (let i = 0; i<squareEls.length; i++){
+//     evt.addEventListener("click", handleClick(squareEls[i]))
+//     // handleClick(squareEls[i])
+//     console.log(squareEls[i])
+// }
+// 6) Handle a player clicking the replay button:
+
